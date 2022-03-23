2bf35124e3edf9b114d7789cdc22801e1e69c0a8
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-23 13:38:25 +0400

    BL готова

---
+++ MyTestMVP_Project.BL/Authorization.cs
@@ -0,0 +1,24 @@
+﻿using MyTestMVP_Project.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestMVP_Project.BL
+{
+    public class Authorization : IAuthorization
+    {
+        public  string AuthorizationPasswor (string password)
+        {
+            if (password == "123")
+            {
+                return "Вы вошли как администратор";
+            }
+            else
+            {
+                return "Я вас не  знаю";
+            }
+        }
+    }
+}
---
+++ MyTestMVP_Project.Core/IAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestMVP_Project.Core
+{
+    public interface IAuthorization
+    {
+        string AuthorizationPasswor(string password);
+    }
+}
