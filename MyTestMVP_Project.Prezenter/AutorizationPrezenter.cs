0507e4773012b6f38ed2cc011f0889e99f49a735
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-23 13:51:10 +0400

    Пишем презентер

---
+++ MyTestMVP_Project.Prezenter/AutorizationPrezenter.cs
@@ -0,0 +1,24 @@
+﻿using MyTestMVP_Project.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestMVP_Project.Prezenter
+{
+    public class AutorizationPrezenter
+    {
+        private IAuthorization _bl;
+        private IViewInterface _view;
+
+        public AutorizationPrezenter(IViewInterface view , IAuthorization bl )
+        {
+            _bl = bl;
+            _view = view;
+
+        }
+
+
+    }
+}
