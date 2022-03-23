83f9f1153cefe9250f528cbfbaa36d83c088832b
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-23 12:24:31 +0400

    Добавьте файлы проекта.

---
+++ MyTestMVP_Project.ViewConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTestMVP_Project.ViewConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
